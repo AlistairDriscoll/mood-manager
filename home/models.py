763f898873c6aaecce496b79cb2c9bb28fc6f2ed
Alistair Driscoll <pallythecoder@gmail.com>
2026-02-26 17:06:12 +0000

    Add: user functionality using allauth, MoodEntry model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class MoodEntry(models.Model):
+    # Kaggle rows: user = None
+    # Real user submissions: user = that user
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name="mood_entries",
+    )
+
+    # Numeric fields (stored as decimals for consistent precision)
+    sleep_hours = models.DecimalField(max_digits=6, decimal_places=3)
+    screen_time = models.DecimalField(max_digits=6, decimal_places=3)
+    exercise_minutes = models.DecimalField(max_digits=8, decimal_places=3)
+
+    daily_pending_tasks = models.PositiveSmallIntegerField()
+
+    interruptions = models.DecimalField(max_digits=6, decimal_places=3)
+    fatigue_level = models.DecimalField(max_digits=6, decimal_places=3)
+    social_hours = models.DecimalField(max_digits=6, decimal_places=3)
+    coffee_cups = models.DecimalField(max_digits=5, decimal_places=2)
+
+    # Categoricals
+    DIET_CHOICES = [
+        ("poor", "Poor"),
+        ("average", "Average"),
+        ("good", "Good"),
+    ]
+    diet_quality = models.CharField(max_length=10, choices=DIET_CHOICES)
+
+    WEATHER_CHOICES = [
+        ("sunny", "Sunny"),
+        ("rainy", "Rainy"),
+        ("cloudy", "Cloudy"),
+        ("snowy", "Snowy"),
+        ("hail", "Hail"),
+        ("foggy", "Foggy"),
+    ]
+    weather = models.CharField(max_length=10, choices=WEATHER_CHOICES)
+
+    # Outcomes / targets
+    mood_score = models.DecimalField(max_digits=5, decimal_places=2)
+    stress_level = models.DecimalField(max_digits=6, decimal_places=3)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        who = self.user.username if self.user else "dataset"
+        return f"{who}: mood {self.mood_score}, stress {self.stress_level}"
--- mood_manager/settings.py
+++ mood_manager/settings.py
@@ -38,14 +38,22 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'home',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+
+    # Allauth
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+
+    # my apps
+    "home",
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -76,6 +84,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mood_manager.wsgi.application'
 
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+    "allauth.account.auth_backends.AuthenticationBackend",
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = "username"
+ACCOUNT_EMAIL_REQUIRED = False
+ACCOUNT_USERNAME_REQUIRED = True
+ACCOUNT_EMAIL_VERIFICATION = "none"
+
+LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/"
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
