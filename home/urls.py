117f272df439be8418b54fb79340fb76db0876d8
Alistair Driscoll <pallythecoder@gmail.com>
2026-02-27 16:14:36 +0000

    add: correct case for urls

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("predict/", views.predict_view, name="predict"),
-    path("improve_mood/", views.improve_my_mood, name="improve_mood"),
-    path("reduce_stress/", views.reduce_stress, name="reduce_stress"),
-    path("model_overview/", views.model_overview, name="model_overview"),
+    path("improve-mood/", views.improve_my_mood, name="improve_mood"),
+    path("reduce-stress/", views.reduce_stress, name="reduce_stress"),
+    path("model-overview/", views.model_overview, name="model_overview"),
 ]
