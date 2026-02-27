beff6147fce094fb95d9744b41fb2e755be5dd2c
Pete <pete@petedanielsmith.co.uk>
2026-02-27 10:06:59 +0000

    Add some basic bootstrap styling and sliders to the form

--- home/forms.py
+++ home/forms.py
@@ -1,6 +1,7 @@
 # forms.py
 from django import forms
 
+
 WEATHER_CHOICES = [
     ("snowy", "Snowy"),
     ("sunny", "Sunny"),
@@ -15,17 +16,100 @@ DIET_CHOICES = [
 ]
 
 class PredictionForm(forms.Form):
-    sleep_hours = forms.FloatField(min_value=0, max_value=24)
-    screen_time = forms.FloatField(min_value=0, max_value=24)
-    exercise_minutes = forms.FloatField(min_value=0, max_value=300)
-    daily_pending_tasks = forms.IntegerField(min_value=0, max_value=100)
-    interruptions = forms.IntegerField(min_value=0, max_value=100)
-    fatigue_level = forms.FloatField(min_value=0, max_value=10)
-    social_hours = forms.FloatField(min_value=0, max_value=24)
-    coffee_cups = forms.IntegerField(min_value=0, max_value=20)
+    sleep_hours = forms.FloatField(
+        min_value=0,
+        max_value=12,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "0.1",
+            "min": "0",
+            "max": "12"
+        })
+    )
+    
+    screen_time = forms.FloatField(
+        min_value=0, max_value=16,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "0.1",
+            "min": "0",
+            "max": "16"
+        })
+    )
 
-    diet_quality = forms.ChoiceField(choices=DIET_CHOICES)
-    weather = forms.ChoiceField(choices=WEATHER_CHOICES)
+    # Sliders: use RangeInput + bootstrap form-range
+    exercise_minutes = forms.FloatField(
+        min_value=0, max_value=180,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "10",
+            "min": "0",
+            "max": "180"
+        })
+    )
+
+    daily_pending_tasks = forms.IntegerField(
+        min_value=0, max_value=20,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "1",
+            "min": "0",
+            "max": "20"
+        })
+    )
+    interruptions = forms.IntegerField(
+        min_value=0, max_value=30,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "1",
+            "min": "0",
+            "max": "30"
+        })
+    )
+    fatigue_level = forms.FloatField(
+        min_value=0, max_value=10,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "0.1",
+            "min": "0",
+            "max": "10"
+        })
+    )
+    social_hours = forms.FloatField(
+        min_value=0, max_value=10,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "0.1",
+            "min": "0",
+            "max": "10"
+        })
+    )
+    coffee_cups = forms.IntegerField(
+        min_value=0, max_value=10,
+        widget=forms.NumberInput(attrs={
+            "type": "range",
+            "class": "form-range",
+            "step": "1",
+            "min": "0",
+            "max": "10"
+        })
+    )
+
+    diet_quality = forms.ChoiceField(
+        choices=DIET_CHOICES,
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
+    weather = forms.ChoiceField(
+        choices=WEATHER_CHOICES,
+        widget=forms.Select(attrs={"class": "form-select"})
+    )
 
     def clean_diet_quality(self):
         return self.cleaned_data["diet_quality"].strip().lower()
