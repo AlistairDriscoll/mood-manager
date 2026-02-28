f4240934a15beef35f49e62c767446e8d22afb1a
Anthony Radose <asradose@gmail.com>
2026-02-28 12:13:02 +0000

    Remove standalone model overview page

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("predict/", views.predict_view, name="predict"),
     path("improve-mood/", views.improve_my_mood, name="improve_mood"),
     path("reduce-stress/", views.reduce_stress, name="reduce_stress"),
-    path("model-overview/", views.model_overview, name="model_overview"),
 ]
